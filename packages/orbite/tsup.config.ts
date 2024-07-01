e84972e26b80e930bd37ea36f6b049aa67743592
Sergey Sova <mail@sergeysova.com>
2024-07-01 17:27:15 +0400

    fix: remove stale imports

--- packages/orbite/tsup.config.ts
+++ packages/orbite/tsup.config.ts
@@ -2,15 +2,13 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: {
+    react: "src/react.ts",
     router: "src/router.ts",
     patronum: "src/patronum.ts",
     "storage/index": "src/storage/index.ts",
     "storage/local": "src/storage/local.ts",
     "storage/query": "src/storage/query.ts",
     "storage/session": "src/storage/session.ts",
-    factorio: "src/factorio.ts",
-    reflect: "src/reflect.ts",
-    react: "src/react.ts",
   },
   outExtension({ format }) {
     return { js: format === "cjs" ? ".cjs" : ".js" };
