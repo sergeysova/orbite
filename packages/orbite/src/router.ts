eea3e0d7c228d0dc4df645141a50f10177e6f20a
Sergey Sova <mail@sergeysova.com>
2024-07-04 00:30:06 +0400

    feat(router): export createRoutesMap and type RoutesMap

--- packages/orbite/src/router.ts
+++ packages/orbite/src/router.ts
@@ -1,2 +1,12 @@
 export type * from "atomic-router";
 export * from "atomic-router";
+
+import { UnmappedRouteObject } from "atomic-router";
+export type RoutesMap = UnmappedRouteObject<any>[];
+
+/**
+ * In the future, this function will be used to validate type parameters of routes and paths in routesMap on the type-level.
+ */
+export function createRoutesMap(routes: [...UnmappedRouteObject<any>[]]): RoutesMap {
+  return routes;
+}
